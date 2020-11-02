5d839fe3ad520575de8f91f09b18c2629a42c379
Adrian Nesvik <adrian.nesvik@lyse.net>
2020-11-02 10:37:25 +0100

    Add components

---
+++ src/components/List.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import Card from './Card'
+
+export default () => {
+    return (
+        <div>
+            List
+            <Card />
+        </div>
+    )
+}
---
+++ src/components/Ranker.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import List from './List';
+import Form from './Form'
+
+export default () => {
+    return (
+        <div>
+            <List />
+            <Form />
+        </div>
+    )
+}
